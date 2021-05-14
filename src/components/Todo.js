d6616ea62dabd34f0f89bbb96faff57032fa7836
Jaggehn <jaggehns@gmail.com>
2021-05-15 00:21:43 +0800

    Implemented delete functionality

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,13 +1,21 @@
 import React from "react";
 
-function Todo({ text }) {
+function Todo({ text, todo, todos, setTodos }) {
+  //Events
+  const deleteHandler = () => {
+    setTodos(todos.filter((el) => el.id !== todo.id));
+    //comparing element that is clicked on with
+    //the todo.id in the todos array
+    //if match, remove the element
+  };
+
   return (
     <div className="todo">
       <li className="todo-item">{text}</li>
       <button className="complete-btn">
         <i className="fas fa-check"></i>
       </button>
-      <button className="trash-btn">
+      <button onClick={deleteHandler} className="trash-btn">
         <i className="fas fa-trash"></i>
       </button>
     </div>
