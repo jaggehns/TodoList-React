87181367ce1906a33a8ea82802a5134d6b436e3d
Jaggehn <jaggehns@gmail.com>
2021-05-15 00:31:03 +0800

    Implemented completed btn functionality

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -9,10 +9,26 @@ function Todo({ text, todo, todos, setTodos }) {
     //if match, remove the element
   };
 
+  const completeHandler = () => {
+    setTodos(
+      todos.map((item) => {
+        if (item.id === todo.id) {
+          return {
+            ...item,
+            completed: !item.completed,
+          };
+        }
+        return item;
+      })
+    );
+  };
+
   return (
     <div className="todo">
-      <li className="todo-item">{text}</li>
-      <button className="complete-btn">
+      <li className={`todo-item ${todo.completed ? "completed" : ""}`}>
+        {text}
+      </li>
+      <button onClick={completeHandler} className="complete-btn">
         <i className="fas fa-check"></i>
       </button>
       <button onClick={deleteHandler} className="trash-btn">
